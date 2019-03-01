05c6f4f703789a57423e00da4b20c825be8f3af0
lu64bag3 <gerald.mathias@lrz.de>
2019-03-01 16:04:43 +0100

    applied patch 4620 4621

--- CPMD.ORG/src/pw_hfx.mod.F90
+++ CPMD.ORG/src/pw_hfx.mod.F90
@@ -217,8 +217,6 @@ CONTAINS
     IF (cntl%use_scaled_hfx) THEN
        IF (.NOT. scex%init) CALL stopgm(procedureN,'ScEX requested, but not initialised',&
                                         __LINE__,__FILE__)
-       IF (cntl%tlsd) CALL stopgm(procedureN,'ScEX for LSD NYI',&
-                                  __LINE__,__FILE__)
        CALL setfftn(scex_ID_parent)
     ELSE
        CALL setfftn(ipoolhfx)
